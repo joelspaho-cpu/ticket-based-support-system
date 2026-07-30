85a47d704ae841efcf0b42c7489c4af05dec9a67
joelspaho-cpu <joelspaho@icloud.com>
2026-07-30 15:43:44 +0200

    add emailaddress attribute to email and add limits to the lengths of all relevant properties

--- TicketSupportSystem/models/User.cs
+++ TicketSupportSystem/models/User.cs
@@ -3,15 +3,16 @@ using System.ComponentModel.DataAnnotations;
 public class User
 {
     public int UserID {get; set;}
-    [Required]
+    [Required, MaxLength(50)]
     public required string DisplayName {get; set;}
-    [Required]
+    [Required, EmailAddress, MaxLength(254)]
     public required string Email {get; set;}
+    [MaxLength(100)]
     public required string PasswordHash {get; set;}
     public bool Has2fa {get; set;}
-    [Required]
+    [Required, MaxLength(10)]
     public required string Region {get; set;}
-    [Required]
+    [Required, MaxLength(10)]
     public required string Language {get; set;}
     public DateTime CreatedAt {get; set;}
     public bool RemainSignedIn {get; set;}
