b9cc8fffa913f117dea86b97f7324d317adedc5f
joelspaho-cpu <joelspaho@icloud.com>
2026-08-09 17:28:37 +0200

    Add first user register view with validation and basic styling

--- TicketSupportSystem/Pages/UserView/Register.cshtml.cs
+++ TicketSupportSystem/Pages/UserView/Register.cshtml.cs
@@ -7,33 +7,34 @@ namespace TicketSupportSystem.Pages.UserView
     public class RegisterModel : PageModel
     {
         [BindProperty]
-        [Required, MaxLength(50)]
+        [Required(ErrorMessage = "Please enter a valid display name"), MaxLength(50)]
         public string DisplayName {get; set;} = string.Empty;
         [BindProperty]
-        [Required, EmailAddress, MaxLength(254)]
+        [Required(ErrorMessage = "Please enter a valid email"), EmailAddress, MaxLength(254)]
         public string Email {get; set;} = string.Empty;
         [BindProperty]
-        [Required, DataType(DataType.Password), MinLength(8), MaxLength(50)]
+        [Required(ErrorMessage = "Please enter a valid password"), DataType(DataType.Password), MinLength(8), MaxLength(100)]
         public string Password {get; set;} = string.Empty;
         [BindProperty]
-        [Required, DataType(DataType.Password), Compare(nameof(Password))]
+        [Required(ErrorMessage = "Please ensure the passwords match"), DataType(DataType.Password), Compare(nameof(Password)), MinLength(8), MaxLength(100)]
         public string ConfirmPassword {get; set;} = string.Empty;
         [BindProperty]
         public bool Has2fa {get; set;}
         [BindProperty]
-        [Required, MaxLength(10)]
+        [Required(ErrorMessage = "Please select your region from the dropdown list"), MaxLength(10)]
         public string Region {get; set;} = string.Empty;
         [BindProperty]
-        [Required, MaxLength(10)]
+        [Required(ErrorMessage = "Please select your language from the dropdown list"), MaxLength(10)]
         public string Language {get; set;} = string.Empty;
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
         }
         public IActionResult OnPost()
         {
           if (!ModelState.IsValid) return Page();
           // once the info is correct we hash the password, save the user to database and redirect to dashboard
-          return Page();
+          return RedirectToPage("Dashboard");
         }
     }
 }
