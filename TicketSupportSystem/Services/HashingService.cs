6980c6ccf5ab5c993859da71abe6844d5ca951a2
joelspaho-cpu <joelspaho@icloud.com>
2026-08-27 01:57:53 +0200

    Fix dummy hash, requests are equal regardless of whether an account exists or not

--- TicketSupportSystem/Services/HashingService.cs
+++ TicketSupportSystem/Services/HashingService.cs
@@ -5,10 +5,10 @@ namespace TicketSupportSystem.Services;
 public class HashingService : IHashingService
 {
     private readonly PasswordHasher<object> _hasher = new();
-    private readonly string DummyHash = "$pbkdf2-sha256$600000$dK8ClGIYvapf8+QMblgQfg==$y9+tHcGxUjE0ANzusrNJVCm73Q6sNBULieLO69jBrHg=";
+    private readonly string DummyHash = "AQAAAAIAAYagAAAAEFX7Tn8vXDyUIW+AEdCj3PQbmgPZDJhyAOxmevE3N6ad/jlBNzK16zf4tsMRXZPaug==";
     
     public HashCheckResult DummyHashVerify(string password){
-         var result = _hasher.VerifyHashedPassword(new object(), password, DummyHash);
+         var result = _hasher.VerifyHashedPassword(new object(), DummyHash, password);
          return result switch
         {
             PasswordVerificationResult.Success => HashCheckResult.Success,
